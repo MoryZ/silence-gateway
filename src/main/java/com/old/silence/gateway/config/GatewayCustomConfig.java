1117c327f6882304cfe7e53f728f85c060d8f1c0
张孟伟 <1436373010@qq.com>
2025-11-23 23:26:34 +0800

    222

--- src/main/java/com/old/silence/gateway/config/GatewayCustomConfig.java
+++ src/main/java/com/old/silence/gateway/config/GatewayCustomConfig.java
@@ -6,11 +6,15 @@ import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Objects;
+
 /**
  * @author moryzang
  */
 @Configuration
 public class GatewayCustomConfig {
+
+    private static final String X_User_ID = "X-User-ID";
 
     /**
      * 基于用户的限流键解析器
@@ -18,7 +22,7 @@ public class GatewayCustomConfig {
     @Bean
     public KeyResolver userKeyResolver() {
         return exchange -> Mono.justOrEmpty(
-                        exchange.getRequest().getHeaders().getFirst("X-User-ID"))
+                        exchange.getRequest().getHeaders().getFirst(X_User_ID))
                 .defaultIfEmpty("anonymous"); // 如果没有用户ID，则使用匿名用户
     }
 
@@ -28,6 +32,6 @@ public class GatewayCustomConfig {
     @Bean
     public KeyResolver ipKeyResolver() {
         return exchange -> Mono.just(
-                exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+                Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress());
     }
 }
